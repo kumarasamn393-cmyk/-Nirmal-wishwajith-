18821d17a7ed5e826a7266f63ec4bfc770c2ac44
kumarasamn393-cmyk <kumarasamn393@gmail.com>
2025-11-05 10:43:39 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/kumarasamn393-cmyk/-Nirmal-wishwajith-/blob/main/images/OIP.webp?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 WISHWA-MD Is Alive Now😍*",
+BOT_OWNER: '94769787408',  // Replace with the owner's phone number
 
 
 
